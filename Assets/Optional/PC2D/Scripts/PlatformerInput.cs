9df59cb564532b994fc61604b456345fcad4ca65
Vadim <vadim.vorobyov.by@gmail.com>
2019-04-26 23:42:27 +0300

    Update to Unity 2019.1.0f2
    removed all default packages from the project
    added controller script to optional/PC2D instead of the missing one.
    most assets were unchanged. serialization will happen on save so there will be .unity files changes later on
    ToDo: SingleScene game concept looks kinda obsolete. unity 2019 has additive scenes, nested prefabs and more. This can mean some serious overhaul of this project.
    ToDo: Review plugins. Unity actively buys plugins and incorporates them into the engine.

---
+++ Assets/Optional/PC2D/Scripts/PlatformerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+//this is not the original file, but a replica. 
+
+[RequireComponent(typeof(PlatformerMotor2D))]
+public class PlatformerInput : MonoBehaviour
+{
+    [SerializeField]
+    string XMovementAxis = "Horizontal";
+    [SerializeField]
+    string JumpAxis = "Jump";
+    [SerializeField]
+    string DashAxis = "Fire1";
+
+    private PlatformerMotor2D motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlatformerMotor2D>();
+    }
+
+    void Update()
+    {
+        motor.normalizedXMovement = Input.GetAxis(XMovementAxis);
+        if(Input.GetAxis(JumpAxis) > 0.0f)
+        {
+            motor.Jump();
+        }
+        if (Input.GetAxis(DashAxis) > 0.0f)
+        {
+            motor.Dash();
+        }
+    }
+}
